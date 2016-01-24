update corrected snippet classifier to use neutral labels, expert uses a default numerical value to return neutral labels, retraining has been updated, pool update has been updated

experiment/base.py
@@ -208,8 +208,8 @@ class Experiment(object):
     def get_name(self):
         cfg = cfgutil.get_section_options(self.config, 'learner')
         post = cfgutil.get_section_option(self.config, 'experiment', 'fileprefix')
-        name = "data-{}-lrn-{}-ut-{}-snip-{}-{}".format(self.dataname, cfg['type'], cfg['loss_function'],
-                                                              cfg['snippet'],  post)
+        name = "data-{}-lrn-{}-ut-{}-{}".format(self.dataname, cfg['type'], cfg['loss_function'],
+                                                               post)
         return name
 
     def bootstrap(self, pool, bt, train, bt_method=None):
@@ -262,23 +262,31 @@ class Experiment(object):
             results['ora_accu'][iteration].append(oracle)
         except Exception:
             pass
-
+        oracle_text = ""
         if self.verbose:
             if iteration == 0:
                 print "\nIT\tACCU\tAUC\tT0\tF1\tF0\tT1"
             print "{0:0.2f}\t{1:.3f}\t{2:.3f}\t".format(iteration, step['accuracy'], step['auc']),
             try:
                 print "\t".join(["{0:.3f}".format(x) for x in np.reshape(oracle, 4)])
+                oracle_text = "\t".join(["{0:.3f}".format(x) for x in np.reshape(oracle, 4)])
             except Exception:
+                oracle_text = ""
                 pass
+
+        with open(self.get_name() + "-accu-all.txt", "a") as f:
+            if iteration == 0:
+               f.write("IT\tACCU\tAUC\tT0\tF1\tF0\tT1\n")
+            to_print = "{0:0.2f}\t{1:.3f}\t{2:.3f}\t{3}\n".format(iteration, step['accuracy'], step['auc'],oracle_text)
+            f.write(to_print)
+
         return results
 
     def update_pool(self, pool, query, labels, train):
         for q, l in zip(query, labels):
             pool.remaining.remove(q[0])
-            if l is not None:
-                train.index.append(q[0])
-                train.target.append(l)
+            train.index.append(q[0])
+            train.target.append(l)
 
         return pool, train
 
@@ -286,7 +294,7 @@ class Experiment(object):
 
         # return learner.fit(X, y, train_index=train.index)
 
-        return learner.fit(pool, train_index=train.index)
+        return learner.fit(pool, train=train)
 
 
     def get_query(self, data, query):
@@ -334,8 +342,10 @@ class Experiment(object):
                 # select query and query labels
                 query_true_labels = pool.target[[di for di, _ in query]]
                 query = learner.next_query(pool, self.step)
+
                 labels = expert.label(self.get_query(pool,query), y=query_true_labels)
 
+                print labels
                 # update pool and cost
                 pool, train = self.update_pool(pool, query, labels, train)
                 current_cost = self.update_cost(current_cost, pool, query)

expert/experts.py
@@ -67,6 +67,7 @@ class ReluctantDocumentExpert(PredictingExpert):
         super(ReluctantDocumentExpert, self).__init__(oracle)
         self.reluctant_threhold = reluctant_threshold
         self.rnd = np.random.RandomState(seed)
+        self.neutral_value = 2
 
     def fit(self, X_text, y=None, vct=None):
         sx = vct.transform(X_text)
@@ -74,7 +75,7 @@ class ReluctantDocumentExpert(PredictingExpert):
         return self
 
     def label(self, data, y=None):
-        prediction = np.array([None] * data.shape[0], dtype=object)
+        prediction = np.array([self.neutral_value] * data.shape[0], dtype=object)
         proba = self.oracle.predict_proba(data)
         pred = self.oracle.predict(data)
         unc = proba.min(axis=1)
@@ -89,6 +90,7 @@ class PerfectReluctantDocumentExpert(PredictingExpert):
         super(PerfectReluctantDocumentExpert, self).__init__(oracle)
         self.reluctant_threhold = reluctant_threshold
         self.rnd = np.random.RandomState(seed)
+        self.neutral_value = 2
 
     def fit(self, X_text, y=None, vct=None):
         sx = vct.transform(X_text)
@@ -99,7 +101,7 @@ class PerfectReluctantDocumentExpert(PredictingExpert):
         data = query
         if isinstance(query, dict):
             data = query.bow
-        prediction = np.array([None] * data.shape[0], dtype=object)
+        prediction = np.array([self.neutral_value] * data.shape[0], dtype=object)
         proba = self.oracle.predict_proba(data)
         unc = 1. - proba.max(axis=1)
         prediction[unc < self.reluctant_threhold] = y[unc < self.reluctant_threhold]
@@ -113,6 +115,7 @@ class TrueReluctantExpert(TrueExpert):
         super(TrueReluctantExpert, self).__init__(oracle)
         self.rnd = np.random.RandomState(seed)
         self.reluctant_p = reluctant_p
+        self.neutral_value = 2
 
     def label(self, data, y=None):
 
@@ -120,6 +123,6 @@ class TrueReluctantExpert(TrueExpert):
 
         coin = self.rnd.random_sample(len(y))  # flip a coin for neutral probability
 
-        prediction[coin < self.reluctant_p] = None  ## if coin is < p then is neutral
+        prediction[coin < self.reluctant_p] = self.neutral_value  ## if coin is < p then is neutral
 
         return prediction

learner/strategy.py
@@ -4,6 +4,7 @@ from sklearn.datasets import base as bunch
 from scipy.sparse import vstack
 from copy import copy
 
+
 class RandomSampling(Learner):
     """docstring for RandomSampling"""
 
@@ -46,6 +47,7 @@ class BootstrapFromEach(Learner):
 
         return chosen
 
+
 class ActiveLearner(Learner):
     """ActiveLearner class that defines a simple utility based pool sampling strategy"""
 
@@ -60,6 +62,7 @@ class ActiveLearner(Learner):
 
 class StructuredLearner(ActiveLearner):
     """StructuredLearner is the Structured reading implementation """
+
     def __init__(self, model, snippet_fn=None, utility_fn=None, seed=1):
         super(StructuredLearner, self).__init__(model, seed=seed)
         import copy
@@ -83,10 +86,10 @@ class StructuredLearner(ActiveLearner):
     def get_name(self):
         return "{}{}".format(self.utility.__name__, self.snippet_utility.__name__)
 
-    def fit(self, data, train_index=None):
+    def fit(self, data, train=None):
         # fit student
-        X = data.bow[train_index]
-        y = data.target[train_index]
+        X = data.bow[train.index]  # vector representation
+        y = train.target ## labels from the oracle
         self.model.fit(X, y)
 
         return self
@@ -111,29 +114,27 @@ class StructuredLearner(ActiveLearner):
     def _snippet_rnd(self, X):
         return self.sent_rnd.random_sample(X.shape[0])
 
-
     def _get_snippets(self, data, candidates):
         ranges = np.cumsum(data.sizes)
-    #     print 0 if i==0 else ranges[i-1],ranges[i]
+        #     print 0 if i==0 else ranges[i-1],ranges[i]
         snips = []
         for i in candidates:
-            snips.append(data.snippets[0 if i==0 else ranges[i-1]:ranges[i]])
+            snips.append(data.snippets[0 if i == 0 else ranges[i - 1]:ranges[i]])
         return snips
 
     def _get_probs_per_snippet(self, data, candidates):
         ranges = np.cumsum(data.sizes)
-    #     print 0 if i==0 else ranges[i-1],ranges[i]
+        #     print 0 if i==0 else ranges[i-1],ranges[i]
         snips = []
         for i in candidates:
-            snips.append(data.snippets[0 if i==0 else ranges[i-1]:ranges[i]])
+            snips.append(data.snippets[0 if i == 0 else ranges[i - 1]:ranges[i]])
         return snips
-
 
     def _compute_snippet(self, pool):
 
         """select the snippet with the best score for each document"""
 
-        x_sent_bow, x_sent, x_len = self._get_snippets(pool) # a matrix of all snippets (stacked), and the size
+        x_sent_bow, x_sent, x_len = self._get_snippets(pool)  # a matrix of all snippets (stacked), and the size
 
         snip_prob = self.snippet_model.predict_proba(x_sent_bow)
 
@@ -148,27 +149,27 @@ class StructuredLearner(ActiveLearner):
 class Joint(StructuredLearner):
     """docstring for Joint"""
 
-    def __init__(self, model, snippet_fn=None, utility_fn=None, minimax=-1,  seed=1):
+    def __init__(self, model, snippet_fn=None, utility_fn=None, minimax=-1, seed=1):
         super(Joint, self).__init__(model, snippet_fn=snippet_fn, utility_fn=utility_fn, seed=seed)
         self.current_training = []
         self.current_training_labels = []
         self.minimax = minimax
-        self.validation_index=[]
+        self.validation_index = []
         self.loss_fn = None
 
     def set_minimax(self, minimax):
-        if minimax =='maximize':
+        if minimax == 'maximize':
             self.minimax = -1
         else:
             self.minimax = 1
-    
+
     def _subsample_pool(self, rem):
         subpool = list(rem)
         self.rnd_state.shuffle(subpool)
         subpool = subpool[:250]
 
         return subpool
-    
+
     def next_query(self, pool, step):
 
         """
@@ -179,25 +180,25 @@ class Joint(StructuredLearner):
         """
         subpool = self._subsample_pool(pool.remaining)
 
-        util = self.expected_utility(pool, subpool) # util list of (utility_score, snipet index of max)
+        util = self.expected_utility(pool, subpool)  # util list of (utility_score, snipet index of max)
 
-        if self.minimax > 0: ## minimizing
+        if self.minimax > 0:  ## minimizing
             max_util = [np.argmin(p) for p in util]  # snippet per document with max/min utility
         else:
             max_util = [np.argmax(p) for p in util]  # snippet per document with max/min utility
 
-        order = np.argsort([util[i][u] for i,u in enumerate(max_util)])[::self.minimax]  # document with snippet utlity max/min
+        order = np.argsort([util[i][u] for i, u in enumerate(max_util)])[
+                ::self.minimax]  # document with snippet utlity max/min
 
         index = [(subpool[i], max_util[i]) for i in order[:step]]
 
         return index
 
-
     def expected_utility(self, data, candidates):
         labels = self.model.classes_
         tra_y = copy(self.current_training_labels)
         tra_x = copy(self.current_training)
-        clf  = copy(self.model)
+        clf = copy(self.model)
         utilities = []  # two per document
         for i, x_i in enumerate(candidates):
             tra_x.append(x_i)
@@ -217,16 +218,16 @@ class Joint(StructuredLearner):
             tra_x = tra_x[:-1]
 
         snippets = self._get_snippets(data, candidates)
-        probs = [self.snippet_model.predict_proba(snip) for snip in snippets] # one per snippet
+        probs = [self.snippet_model.predict_proba(snip) for snip in snippets]  # one per snippet
         cost = data.snippet_cost[candidates]
         exp_util = []
 
-        for ut, pr, co in zip(utilities, probs, cost): # for every document
+        for ut, pr, co in zip(utilities, probs, cost):  # for every document
             exp = []
-            for p,c in zip(pr,co): # for every snippet in the document
+            for p, c in zip(pr, co):  # for every snippet in the document
                 exp.append((p[0] * ut[0][2] + p[1] * ut[1][2]) / c)  ## utility over cost
 
-            exp_util.extend([exp]) # one per snippet
+            exp_util.extend([exp])  # one per snippet
 
         return exp_util
 
@@ -236,7 +237,7 @@ class Joint(StructuredLearner):
 
         return loss
 
-    def fit(self, data, train_index=[]):
+    def fit(self, data, train=[]):
 
         """
         fit an active learning strategy
@@ -245,18 +246,19 @@ class Joint(StructuredLearner):
         :param snippets:
         :return:
         """
-        X = data.bow[train_index]
-        y = data.target[train_index]
+        non_neutral = np.array(train.target) < 2
+        selected = np.array(train.index)[non_neutral]
+        x = data.bow[selected]
+        y = np.array(train.target)[non_neutral]
+        self.model.fit(x, y)
 
-        super(Joint, self).fit(data, train_index=train_index)
-        self.current_training.extend(train_index)
-        self.current_training_labels.extend(y)
+        self.current_training = [i for i,n in zip(train.index, non_neutral) if n]
+        self.current_training_labels = y.tolist()
 
-        snippets = self._get_snippets(data, train_index)
+        snippets = self._get_snippets(data, train.index)
         labels = []
-        for l,s in zip(y, data.sizes[train_index]):
+        for l, s in zip(np.array(train.target), data.sizes[train.index]):
             labels.extend([l] * s)
         self.snippet_model.fit(vstack(snippets), labels)
 
         return self
-

utilities/costutils.py
@@ -1,5 +1,7 @@
 __author__ = 'mramire8'
 
+import numpy as np
+
 
 def unit_cost(query, cost_model=None):
     n = 1
@@ -9,22 +11,25 @@ def unit_cost(query, cost_model=None):
         n = len(query)
     return [1.] * n
 
+
 def word_cost(query, cost_model=None):
     c = 0
     if isinstance(query, int):
         return query
 
-    if isinstance(query, dict):
-        x_text = query.snippet
-    else:
-        x_text = query
-    if isinstance(x_text, unicode):
+    x_text = query
+    if isinstance(x_text, basestring):
         c = len(x_text.split())
-    elif isinstance(x_text, list):
+    elif isinstance(x_text, list) or isinstance(x_text, np.array):
         if x_text is not None:
             c = [len(x.split()) for x in x_text]
 
     return c
+
+
+def log_word_cost(query, cost_model=None):
+    return np.log2(1+word_cost(query, cost_model=cost_model))
+
 
 def intra_cost(query, cost_model=None):
     if cost_model is None:
@@ -46,18 +51,18 @@ def intra_cost(query, cost_model=None):
 def _cost_intrapolated(x, cost, kvalues):
     import numpy as np
 
-    binx = min(np.digitize([x], kvalues)[0], len(cost)-1)
-    lbbinx = max(binx-1, 0)
+    binx = min(np.digitize([x], kvalues)[0], len(cost) - 1)
+    lbbinx = max(binx - 1, 0)
 
-    y1 = cost[lbbinx] if lbbinx>=0  else 0
+    y1 = cost[lbbinx] if lbbinx >= 0  else 0
     y2 = cost[binx]
-    x1 = kvalues[lbbinx] if lbbinx >=0 else 0
+    x1 = kvalues[lbbinx] if lbbinx >= 0 else 0
     x2 = kvalues[binx]
 
-    if (x2-x1) == 0:
+    if (x2 - x1) == 0:
         return cost[0]
 
-    m = (y2-y1) / (x2-x1)
+    m = (y2 - y1) / (x2 - x1)
     b = y2 - m * x2
 
     if x < kvalues[0]: