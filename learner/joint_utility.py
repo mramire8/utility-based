fixed utility computation for actual improvement of the classifier from current performance

learner/joint_utility.py
@@ -41,7 +41,6 @@ class Joint(StructuredLearner):
         return [queries[i][0] for i in reorder[:50]]
 
     def next_query(self, pool, step):
-
         """
         Select best candidates for asking the oracle.
         :param pool:
@@ -51,14 +50,13 @@ class Joint(StructuredLearner):
         subpool = self._subsample_pool(pool.remaining)
 
         util1 = self.expected_utility(pool, subpool)  # util list of (utility_score, snipet index of max)
-        revisit = self._select_revisit(pool.revisit)
-        rev_util = self.expected_utility(pool, revisit)
-
-        util = util1 + rev_util
-
-        subpool += revisit
-
-
+        if False:
+            revisit = self._select_revisit(pool.revisit)
+            rev_util = self.expected_utility(pool, revisit)
+            util = util1 + rev_util
+            subpool += revisit
+        else:
+            util = util1
 
         if self.minimax > 0:  ## minimizing
             max_util = [np.argmin(p) for p in util]  # snippet per document with max/min utility
@@ -72,7 +70,13 @@ class Joint(StructuredLearner):
         return index[:step]
 
     def expected_utility(self, data, candidates):
-
+        """
+        Compute the expected utility of each candidate instance
+        :param data: bunch of the pool
+        :param candidates: list of candidates
+        :return: list of all expected utilities per snippet per document
+        """
+        curr = self.current_utility(copy(self.model), data)
         utilities = self.compute_utility_per_document(data, candidates)
 
         snippets = self._get_snippets(data, candidates)
@@ -83,13 +87,39 @@ class Joint(StructuredLearner):
         for ut, pr, co in zip(utilities, probs, cost):  # for every document
             exp = []
             for p, c in zip(pr, co):  # for every snippet in the document
-                exp.append((p[0] * ut[0][2] + p[1] * ut[1][2]) / c)  ## utility over cost, ignore lbl =2
+                exp.append((p[:2] * [ut[0][2]-curr, ut[1][2]-curr]).sum() / c)  ## utility over cost, ignore lbl =2
+                exp.append(np.dot(p[:2],[ut[0][2]-curr, ut[1][2]-curr]) / c)  ## utility over cost, ignore lbl =2
 
             exp_util.extend([exp])  # one per snippet
 
         return exp_util
 
+    def current_utility(self, clf, data):
+
+        """
+        Compute the current utility of the student model
+        :param clf:
+        :param data:
+        :return:
+        """
+        tra_y = copy(self.current_training_labels)
+        tra_x = copy(self.current_training)
+
+        if self.validation_method == 'cross-validation':
+            return self.evaluation_on_validation(clf, data.bow[tra_x], np.array(tra_y), method=self.validation_method)
+        else:
+            clf.fit(data.bow[tra_x], tra_y)
+            return self.evaluation_on_validation(clf, data.validation_set.bow[data.validation],
+                                                np.array(data.validation_set.target), method=self.validation_method)
+
     def compute_utility_per_document(self, data, candidates):
+        """
+        For ever document in the candidate list, compute the utility of adding the document ot the training set with every
+        possible label.
+        :param data:
+        :param candidates:
+        :return: List of utility per label of each candidate
+        """
         labels = self.model.classes_
         tra_y = copy(self.current_training_labels)
         tra_x = copy(self.current_training)
@@ -106,39 +136,48 @@ class Joint(StructuredLearner):
         return [self.snippet_model.predict_proba(snip) for snip in snippets]
 
     def evaluation_on_validation_label(self, lbl, data, tra_x, tra_y, i):
+
         if lbl < 2: # if not neutral
+            clf = copy(self.model)
             x = tra_x
             y = tra_y + [lbl]
-            clf = copy(self.model)
-            clf.fit(data.bow[x], y)
-            # res = self.evaluation_on_validation(clf, data.validation_set.bow[data.validation], data.validation_set.target[data.validation])
-            if data.validation_set.bow[data.validation].shape[0] != len(data.validation_set.target):
-                raise Exception("Oops, the validation data has problems")
-            res = self.evaluation_on_validation(clf, data.validation_set.bow[data.validation], np.array(data.validation_set.target))
-            return (i, lbl, res)
+            if self.validation_method == 'cross-validation':
+                res = self.evaluation_on_validation(clf, data.bow[x], np.array(y), method=self.validation_method)
+
+            else:
+
+                clf.fit(data.bow[x], y)
+                if data.validation_set.bow[data.validation].shape[0] != len(data.validation_set.target):
+                    raise Exception("Oops, the validation data has problems")
+                res = self.evaluation_on_validation(clf, data.validation_set.bow[data.validation],
+                                                    np.array(data.validation_set.target), method=self.validation_method)
+            return i, lbl, res
         else:
             # utility of neutral label
-            return (i, lbl, 0)
+            return i, lbl, 0
 
-    def cv_probability(self, clf, data, target, n_folds=10):
+    def cross_validation_utility(self, clf, data_bow, target, n_folds=10):
+
         skf = StratifiedKFold(target, n_folds=n_folds)
         scores = []
         for train_index, test_index in skf:
-            X_train, X_test = data[train_index], data[test_index]
+            X_train, X_test = data_bow[train_index], data_bow[test_index]
             y_train, y_test = target[train_index], target[test_index]
             clf.fit(X_train, y_train)
-            predictions = clf.predict_proba(X_test)
-            scores.extend([predictions[i][j] for i,j in enumerate(y_test)])
+            loss = self.loss_fn(clf, X_test, y_test)
+            scores.append(loss)
+            # predictions = clf.predict_proba(X_test)
+            # scores.extend([predictions[i][j] for i,j in enumerate(y_test)])
         return np.sum(scores)
 
-    def evaluation_on_validation(self, clf, data, target):
-        from sklearn.metrics import fbeta_score, make_scorer
-        if self.validation_method == 'cross-validation':
-            # predicted = cross_val_predict(clf, data, target, cv=10)
-            # return accuracy_score(target, predicted)
-            return self.cv_probability(clf, data, target, n_folds=10)
+    def evaluation_on_validation(self, clf, data_bow, target, method=None):
+        if method is None:
+            raise Exception("Oops, we need the method to compute the score")
+
+        if method == 'cross-validation':
+            return self.cross_validation_utility(clf, data_bow, target, n_folds=10)
         else:
-            return self.loss_fn(clf, data, target)
+            return self.loss_fn(clf, data_bow, target)
 
     @staticmethod
     def _safe_fit(model, x, y, labels=None):
@@ -152,6 +191,13 @@ class Joint(StructuredLearner):
 
     def _get_query_snippets(self, data, candidates):
 
+        """
+        Get a snippet of each candidate, as seen by the oralce.
+        :rtype: tuple
+        :param data: bunch with pool candidates
+        :param candidates:
+        :return: snippets, targets
+        """
         if hasattr(data.snippets, "shape"):
             ranges = np.cumsum(data.sizes)
             queries = []
@@ -183,9 +229,36 @@ class Joint(StructuredLearner):
 
         return queries, targets
 
+    def _get_query_snippets_v0(self, data, candidates):
 
+        """
+        Get all snippets of each candidate, all possible snippets (according to the snippet function) for each document
+        :param data:
+        :param candidates:
+        :return:
+        """
+        if hasattr(data.snippets, "shape"):
+            ranges = np.cumsum(data.sizes)
+            queries = []
+            targets = []
+            for di, si, ti in zip(candidates.index, candidates.snip, candidates.target):
+                snippets = data.snippets[0 if di == 0 else ranges[di-1]:ranges[di]]
+                queries.extend(snippets)
+                targets.extend([ti] * len(snippets))
+        else:
+            # queries = [data.snippets[d][np.ix_([s])] for d,s in zip(candidates.index, candidates.snip)]
+            queries = []
+            targets = []
+            for di, si, ti in zip(candidates.index, candidates.snip, candidates.target):
+                snippets = data.snippets[di]
+                queries.append(snippets)
+                if hasattr(snippets, "shape"):
+                    n = snippets.shape[0]
+                else:
+                    n = len(snippets)
+                targets.extend([ti] * n)
 
-        # return snips
+        return queries, targets
 
     def _get_query_labels(self, target, candidates):
         pass
@@ -205,7 +278,6 @@ class Joint(StructuredLearner):
         y = np.array(train.target)[non_neutral]
 
         self.model = self._safe_fit(self.model, x, y)
-            # self.model.fit(x, y)
 
         self.current_training = [i for i,n in zip(train.index, non_neutral) if n]
         self.current_training_labels = y.tolist()

learner/utility_based.py
@@ -20,12 +20,12 @@ class UtilityBasedLearner(Joint):
     def loss_conditional_error(self, clf, data, target):
         probs = clf.predict_proba(data)
         loss = np.array([1 - probs[i][j] for i,j in enumerate(target)])
-        return loss.mean()
+        return loss.sum()
 
     def loss_prediction(self, clf, data, target):
         probs = clf.predict_proba(data)
         loss = np.array([probs[i][j] for i,j in enumerate(target)])
-        return loss.mean()
+        return loss.sum()
 
     def loss_error(self, clf, data, target):
         preds = clf.predict(data)